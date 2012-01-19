739d2970f81538b145adf86c7b4bb4a99a2b6b4f
Julie Dickerson <jujudickerson@gmail.com>
2012-01-18 21:40:18 -0500

    Building out pages with old content
    design of homepage almost done (need to figure out banner and page,
    content area), figure out how to represent bubbled minilinks once
    navigated away from homepage, redesign all content to be prettier please

---
+++ js/myJSfile.js
@@ -0,0 +1,15 @@
+
+
+function printButton() {
+	var wnd = window.open('coupon.html');
+}
+
+
+function printButton1() {
+	var wnd = window.open('coupon1.html');
+}
+
+function printButton2() {
+	var wnd = window.open('coupon2.html');
+}
+
